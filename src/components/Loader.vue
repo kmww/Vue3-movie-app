344981b47ed2d3d4bd99f4590eed30cab2527f51
kmww <kmw9708@gmail.com>
2022-06-11 21:43:28 +0900

    change spinner color

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -9,7 +9,8 @@
       absolute, 
       fixed
     }"
-    class="spinner-border text-primary"></div>
+    class="spinner-border">
+  </div>
 </template>
 
 <script>
@@ -36,12 +37,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .spinner-border {
   margin: auto;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
+  color: $primary;
   &.absolute {
     position: absolute;
   }
